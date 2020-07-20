47d1543b7cebc6e653e1acaca4930193d5d065d6
Fabio Verissimo <fabio.meihua08@gmail.com>
2020-07-19 23:10:55 -0300

    CRUD

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const requireDir = require('require-dir');
 //iniciando o APP
 const app = express();
 
+//habilita o POST usando JSON
+app.use(express.json());
+
 //iniciando o DB
 mongoose.connect('mongodb://localhost:27017/nodeapi',{ useNewUrlParser: true, useUnifiedTopology: true} );
 requireDir('./src/models');
