0659278ac0d46909829b517d6b5194401b79170d
Fabio Verissimo <fabio.meihua08@gmail.com>
2020-07-19 21:54:44 -0300

    Primeiro Commit

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const requireDir = require('require-dir');
+
+//iniciando o APP
+const app = express();
+
+//iniciando o DB
+mongoose.connect('mongodb://localhost:27017/nodeapi',{ useNewUrlParser: true, useUnifiedTopology: true} );
+requireDir('./src/models');
+
+
+//const Product = mongoose.model('Product');
+
+//Rotas
+app.use("/api", require("./src/routes"));
+
+
+app.listen(3001);
